cdda307eb7dd53f22f0057fc12bdafe4242fc205
Chandana <56185553+Chandana2901@users.noreply.github.com>
2022-04-13 10:48:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("myRange").value;
+    time = document.getElementById("myRange").value;
+    var currDate = new Date().getFullYear();
+    var future= parseInt(currDate,10) + parseInt(time,10);
+    var interest= principal * time * rate / 100;
+    var result= "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>";
+    document.getElementById("result").innerHTML=result;
 }
-        
+
+function showVal(newVal){
+    document.getElementById("showRate").innerHTML=newVal + '%';
+}
