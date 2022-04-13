ec9f538357ad5f3d9e2333058402241a2da5b0b6
Chandana <56185553+Chandana2901@users.noreply.github.com>
2022-04-13 16:56:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function compute()
 {
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("myRange").value;
-    time = document.getElementById("myRange").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("myRange").value;
+    var time = document.getElementById("myRange").value;
+    
+    if (principal == "") { //Here Check is empty
+        alert("Amount can't by empty or alphabet character");
+        document.getElementById("principal").focus();
+        return false;
+    }else{
+        if (principal <= 0) { //Here check is a positive number
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+            return false;
+        }
+        
     var currDate = new Date().getFullYear();
     var future= parseInt(currDate,10) + parseInt(time,10);
     var interest= principal * time * rate / 100;
